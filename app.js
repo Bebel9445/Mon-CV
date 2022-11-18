0355dba8dcc3a853d67f10fc8f6645b5a742c990
Bebel9445 <115141738+Bebel9445@users.noreply.github.com>
2022-11-19 00:12:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body");
+const scrollbar = document.querySelector(".scroll")
+
+
+window.addEventListener("scroll", () => {
+    let scroll = window.scrollY / (body.offsetHeight - window.innerHeight);
+    let scrollpercent = Math.round(scroll * 100);
+    scrollbar.style.width = scrollpercent + "%"
+})
