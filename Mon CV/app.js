505171ce41ff521fc880b6d2f5e1356023ab525e
Bebel9445 <bebel9445@gmail.com>
2022-11-18 23:09:13 +0100

    first push

--- Mon CV/app.js
+++ Mon CV/app.js
@@ -1,7 +1,6 @@
 const body = document.querySelector("body");
 const scrollbar = document.querySelector(".scroll")
 
-
 window.addEventListener("scroll", () => {
     let scroll = window.scrollY / (body.offsetHeight - window.innerHeight);
     let scrollpercent = Math.round(scroll * 100);
