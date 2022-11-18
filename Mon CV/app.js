480171b04676cb9c7376c74ff7ddb8c769fed17a
Bebel9445 <115141738+Bebel9445@users.noreply.github.com>
2022-11-19 00:09:29 +0100

    Delete Mon CV directory

--- Mon CV/app.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body");
-const scrollbar = document.querySelector(".scroll")
-
-
-window.addEventListener("scroll", () => {
-    let scroll = window.scrollY / (body.offsetHeight - window.innerHeight);
-    let scrollpercent = Math.round(scroll * 100);
-    scrollbar.style.width = scrollpercent + "%"
-})
